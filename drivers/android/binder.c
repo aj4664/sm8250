2fe4ed7e5be7f6efd0ae92589ecd329896fd0440
Yanfeng Lee <liyafe1997@126.com>
2024-08-01 16:57:38 +0000

    Fix build problem. Revert to CONFIG_MI_SCHED from pipa-oss

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -94,7 +94,7 @@
 #include <linux/trace_clock.h>
 #endif
 
-#ifdef CONFIG_PACKAGE_RUNTIME_INFO
+#ifdef CONFIG_MI_SCHED
 extern void mi_binder_restore_vip_hook(struct binder_transaction *in_reply_to, struct task_struct *task);
 extern void mi_binder_set_vip_hook(struct binder_transaction *t, struct task_struct *task);
 #endif
@@ -1310,7 +1310,7 @@ static void binder_transaction_priority(struct task_struct *task,
 
 	binder_set_priority(task, desired_prio);
 
-#ifdef CONFIG_PACKAGE_RUNTIME_INFO
+#ifdef CONFIG_MI_SCHED
 	mi_binder_set_vip_hook(t, task);
 #endif
 
@@ -3673,7 +3673,7 @@ static void binder_transaction(struct binder_proc *proc,
 #ifdef CONFIG_BINDER_OPT
 		binder_thread_restore_inherit_top_app(thread);
 #endif
-#ifdef CONFIG_PACKAGE_RUNTIME_INFO
+#ifdef CONFIG_MI_SCHED
 		mi_binder_restore_vip_hook(in_reply_to, current);
 #endif
 
@@ -3793,7 +3793,7 @@ err_invalid_target_handle:
 
 	BUG_ON(thread->return_error.cmd != BR_OK);
 	if (in_reply_to) {
-#ifdef CONFIG_PACKAGE_RUNTIME_INFO
+#ifdef CONFIG_MI_SCHED
 		mi_binder_restore_vip_hook(in_reply_to, current);
 #endif
 		binder_restore_priority(current, in_reply_to->saved_priority);
@@ -4420,7 +4420,7 @@ retry:
 		}
 #endif
 
-#ifdef CONFIG_PACKAGE_RUNTIME_INFO
+#ifdef CONFIG_MI_SCHED
 		mi_binder_restore_vip_hook(NULL, current);
 #endif
 		binder_restore_priority(current, proc->default_priority);
