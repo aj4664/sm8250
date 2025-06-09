a79e541017d3789812c1a04f26f15e066f60f5b0
TIMISONG-dev <dima.pilaer@gmail.com>
2025-06-09 19:48:56 +0300

    sched: Remove unused wallclock & redefinition sched_ktime_clock()

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5048,7 +5048,6 @@ void scheduler_tick(void)
 	struct rq *rq = cpu_rq(cpu);
 	struct task_struct *curr = rq->curr;
 	struct rq_flags rf;
-	u64 wallclock;
 	unsigned int flag = 0;
 	unsigned long thermal_pressure;
 
@@ -5056,7 +5055,6 @@ void scheduler_tick(void)
 
 	rq_lock(rq, &rf);
 
-	wallclock = sched_ktime_clock();
 	update_rq_clock(rq);
 	thermal_pressure = arch_scale_thermal_pressure(cpu_of(rq));
 	update_thermal_load_avg(rq_clock_thermal(rq), rq, thermal_pressure);
@@ -5930,7 +5928,6 @@ static void __sched notrace __schedule(unsigned int sched_mode)
 	struct rq_flags rf;
 	struct rq *rq;
 	int cpu;
-	u64 wallclock;
 
 	cpu = smp_processor_id();
 	rq = cpu_rq(cpu);
@@ -6006,7 +6003,6 @@ static void __sched notrace __schedule(unsigned int sched_mode)
 	clear_tsk_need_resched(prev);
 	clear_preempt_need_resched();
 
-	wallclock = sched_ktime_clock();
 	if (likely(prev != next)) {
 
 		rq->nr_switches++;
